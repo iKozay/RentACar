8af3b9e2ac9661a85389e9f20cb750d1bcce00f9
Mohadgb <saidimoha.pro@gmail.com>
2024-02-28 17:23:29 -0500

    wrote db connection file

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,20 +1,52 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const { connect_db } = require("./mongo_conn");
+
 
 const app = express();
 
-app.use(logger('dev'));
+
+// Loading the environment variables from the .env file.
+require("dotenv").config();
+
+
+
+
+app.use(cors());
+
+// Parsing the body to json
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+const port = process.env.PORT || 5001;
+
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-module.exports = app;
+
+
+
+// Router listening for root and responding with  Comptan real estate
+app.get("/", (req, res) => {
+  res.send("GazaBrigade");
+});
+
+connect_db()
+  .then(() => {
+  
+      app.listen(port, console.log(`Server started on port ${port}`));
+
+
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports = { app };

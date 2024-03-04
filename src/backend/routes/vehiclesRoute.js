6fa0c40d3f2b727044929c428b3ae64ef4ac9740
Mohadgb <saidimoha.pro@gmail.com>
2024-03-03 20:35:41 -0500

    added comment

--- src/backend/routes/vehiclesRoute.js
+++ src/backend/routes/vehiclesRoute.js
@@ -6,7 +6,12 @@ const { addVehicle, deleteVehicle } = require('../controllers/vehicleController'
 const router = express.Router();
 
 router.post("/add",addVehicle);
+
+// delete route
 router.delete("/delete/:id",deleteVehicle);
+
+
+
 
 
 

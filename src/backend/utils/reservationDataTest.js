98d6bdb084953f3124810d3ad371fd3263631ed7
codedsami <miskat.mahmud@mail.concordia.ca>
2024-03-10 22:23:17 -0400

    added unit test, to be verified

---
+++ src/backend/utils/reservationDataTest.js
@@ -0,0 +1,10 @@
+const reservation = {
+    vin: "ABC123",
+    reservationDate: new Date(),
+    pickupDate: new Date(),
+    returnDate: new Date(),
+    userID: "65e411c2751c4a87d73f4530", 
+  };
+  
+  module.exports = { reservation };
+  

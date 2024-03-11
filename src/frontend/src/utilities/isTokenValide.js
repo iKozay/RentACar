00663065114bafefd02b26b5b687d536c3cb87e6
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-03-11 10:42:17 -0400

    frontend: implement update customer in dashboard
    now the admin can update a customer

--- src/frontend/src/utilities/isTokenValide.js
+++ src/frontend/src/utilities/isTokenValide.js
@@ -1,11 +1,11 @@
 import fetchData from "./fetchData";
 export default function isTokenValide(){
-    const response = fetchData("http://localhost:3000/api/auth/checkToken",{
+    const response = fetch("http://localhost:3000/api/auth/checkToken",{
         method:"GET",
         headers:{
             "Content-Type":"application/json"
         }
     });
-    if(response.error)return false;
-    return true;
+    if(response.ok)return true;
+    else return false;
 }

ecbeeed2e651efc6cc3854892c839029c68616f3
Zeiad Badawy <zeiadbadawy@gmail.com>
2024-03-10 21:33:01 -0400

    frontend: sorting by num of seats

--- src/frontend/src/components/browsingPage/SortFilterButtons.jsx
+++ src/frontend/src/components/browsingPage/SortFilterButtons.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
 
-export default function SortFilterButtons() {
+export default function SortFilterButtons({setVehicles, vehicles}) {
   
   const [isOpen, setIsOpen] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -11,6 +11,12 @@ export default function SortFilterButtons() {
   };
 
   const handleCategoryClick = (category) => {
+    if(category!==null){
+      const sortedVehicles = vehicles.filter((vehicle) => vehicle.numberOfSeats === category);
+      setVehicles(sortedVehicles);
+    }else{
+        setVehicles(vehicles);
+    }
     setSelectedCategory(category);
     setIsOpen(false);
   };
@@ -23,7 +29,7 @@ export default function SortFilterButtons() {
         {/*Button 1*/}
         <button ref={buttonRef} onClick={toggleDropdown} className="text-white bg-neutral-700 hover:bg-neutral-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
           type="button">
-          {selectedCategory || 'Car Type'}
+          {selectedCategory || 'Number of Seats'}
 
           <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
@@ -45,23 +51,37 @@ export default function SortFilterButtons() {
             <ul className="p-3 space-y-3 text-sm text-gray-700">
               <li>
                 <div
+                    className={`cursor-pointer ${selectedCategory === 'SUV' ? 'font-medium text-neutral-900' : ''}`}
+                    onClick={() => handleCategoryClick(null)}>
+                  None
+                </div>
+              </li>
+              <li>
+                <div
                   className={`cursor-pointer ${selectedCategory === 'SUV' ? 'font-medium text-neutral-900' : ''}`}
-                  onClick={() => handleCategoryClick('SUV')}>
-                  SUV
+                  onClick={() => handleCategoryClick(2)}>
+                  2
                 </div>
               </li>
               <li>
                 <div
                   className={`cursor-pointer ${selectedCategory === 'Sedan' ? 'font-medium text-neutral-900' : ''}`}
-                  onClick={() => handleCategoryClick('Sedan')}>
-                  Sedan
+                  onClick={() => handleCategoryClick(4)}>
+                  4
+                </div>
+              </li>
+              <li>
+                <div
+                    className={`cursor-pointer ${selectedCategory === 'Sport' ? 'font-medium text-neutral-900' : ''}`}
+                    onClick={() => handleCategoryClick(5)}>
+                  5
                 </div>
               </li>
               <li>
                 <div
                   className={`cursor-pointer ${selectedCategory === 'Sport' ? 'font-medium text-neutral-900' : ''}`}
-                  onClick={() => handleCategoryClick('Sport')}>
-                  Sport
+                  onClick={() => handleCategoryClick(6)}>
+                  6
                 </div>
               </li>
             </ul>

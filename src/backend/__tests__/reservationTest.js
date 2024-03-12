bc09ffffda9725e03af6b02998bf841b89500695
Zeiad Badawy <zeiadbadawy@gmail.com>
2024-03-11 22:59:19 -0400

    fix: reservation unit test

--- src/backend/__tests__/reservationTest.js
+++ src/backend/__tests__/reservationTest.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const app = require("../app");
 const Reservation = require('../models/reservationModel');
 const { reservation } = require('../utils/reservationDataTest');
+const {user1} = require("../utils/userDataTest");
 
 require("dotenv").config();
 
@@ -16,17 +17,19 @@ afterEach(async () => {
     await mongoose.connection.close();
 });
 
-// describe('Reservation Routes', () => {
-//     describe('GET /api/reservations/:reservationId', () => {
-//         it('should return a specific reservation', async () => {
-//             const newReservation = await Reservation.create(reservation);
-//             const res = await request(app).get(`/api/reservations/${newReservation._id}`);
-//             expect(res.status).toBe(200);
-//             // Modify the assertion to check if vin is an object with an _id property
-//             expect(res.body.vin._id).toBe("65eb245afc880613982a5caa");
-
-//         });
-//     });
+describe('Reservation Routes', () => {
+    describe('GET /api/reservations/:reservationId', () => {
+        it('should return a specific reservation', async () => {
+            const newReservation = await Reservation.create(reservation);
+            const res = await request(app).get(`/api/reservations/${newReservation._id}`);
+            expect(res.status).toBe(200);
+            // Modify the assertion to check if vin is an object with an _id property
+//            expect(res.body.vin._id).toBe("65eb245afc880613982a5caa");
+            // print body in console
+            console.log(res.body);
+            expect(res.body.vin).toHaveProperty('_id', "65e52b7f3a6b6fac482c8278");
+        });
+    });
 
     describe('GET /api/reservations/user/:userId', () => {
         it('should return all reservations for a specific user', async () => {
@@ -42,11 +45,11 @@ afterEach(async () => {
             expect(res.status).toBe(201);
             
             //expect(res.body.vin).toHaveProperty('_id');
-            expect(res.body).toHaveProperty('vin', "65eb245afc880613982a5caa");
+            expect(res.body).toHaveProperty('vin', "65e52b7f3a6b6fac482c8278");
             expect(res.body).toHaveProperty('reservationDate', new Date("2024/3/8").toISOString());
             expect(res.body).toHaveProperty('pickupDate', new Date("2024/3/9").toISOString());
             expect(res.body).toHaveProperty('returnDate', new Date("2024/3/10").toISOString());
-            expect(res.body).toHaveProperty('userID', "65e411c2751c4a87d73f4530");
+            expect(res.body).toHaveProperty('userID', "65ef29928e591664663d138d");
         });
     });
 
--- src/backend/utils/reservationDataTest.js
+++ src/backend/utils/reservationDataTest.js
@@ -1,9 +1,9 @@
 const reservation = {
-  vin: "65eb245afc880613982a5caa", 
+  vin: "65e52b7f3a6b6fac482c8278",
   reservationDate: new Date("2024/3/8").toISOString(),
   pickupDate: new Date("2024/3/9").toISOString(),
   returnDate: new Date("2024/3/10").toISOString(),
-  userID: "65e411c2751c4a87d73f4530"
+  userID: "65ef29928e591664663d138d"
 };
   module.exports = { reservation };
   

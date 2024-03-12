aaa35124fe966c527fa04a13e04afe0355f4fa45
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-03-12 14:50:27 -0400

    fix: fixed signup form request

--- src/frontend/src/Pages/Signup.jsx
+++ src/frontend/src/Pages/Signup.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import fetchData from "../utilities/fetchData";
 
-export default function Signup () {
+export default function Signup() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     username: "",
@@ -13,7 +13,7 @@ export default function Signup () {
     phone_number: "",
     date_of_birth: "",
     role: "customer",
-    profile_picture: ""
+    profile_picture: "",
   });
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -22,6 +22,7 @@ export default function Signup () {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+    console.log(JSON.stringify(formData));
     const response = await fetchData("http://localhost:3000/api/auth/signup", {
       method: "POST",
       headers: {
@@ -32,7 +33,7 @@ export default function Signup () {
     if (response.data) {
       setLoading(false);
       setSuccess(true);
-      navigate(-1);
+      setTimeout(() => navigate(-1), 1000);
     } else if (response.error) {
       setLoading(false);
       setError(true);
@@ -42,132 +43,141 @@ export default function Signup () {
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   return (
     <div className="max-w-md mx-auto p-6 rounded-md shadow-md bg-neutral-200 mt-10">
-      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-        Sign up for an account
-      </h2>
-      <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-        <div>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            First Name
-          </label>
-          <input
-            type="text"
-            name="first_name"
-            required
-            value={formData.first_name}
-            onChange={handleChange}
-            className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Last Name
-          </label>
-          <input
-            type="text"
-            name="last_name"
-            required
-            value={formData.last_name}
-            onChange={handleChange}
-            className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            required
-            value={formData.email}
-            onChange={handleChange}
-            className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Phone Number
-          </label>
-          <input
-            type="tel"
-            name="phone_number"
-            required
-            value={formData.phone_number}
-            onChange={handleChange}
-            className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Username
-          </label>
-          <input
-            type="text"
-            name="username"
-            required
-            value={formData.username}
-            onChange={handleChange}
-            className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            required
-            value={formData.password}
-            onChange={handleChange}
-            className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Date of Birth
-          </label>
-          <input
-            type="date"
-            name="date_of_birth"
-            required
-            value={formData.date_of_birth}
-            onChange={handleChange}
-            className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium leading-6 text-gray-900">
-            Profile Picture URL
-          </label>
-          <input
-            type="text"
-            name="profile_picture"
-            value={formData.profile_picture}
-            onChange={handleChange}
-            className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="flex w-full justify-center rounded-md bg-neutral-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-neutral-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            disabled={loading}
-          >
-
-          {loading ? "Signing up..." : "Sign Up"}
-    </button>
-  </div>
-</form>
-{error && <div className="text-red-400 text-sm">Error signing up</div>}
-{success && <div>Successfully signed up</div>}
-</div>
-);
-};
+      {!success && (
+        <>
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign up for an account
+          </h2>
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                First Name
+              </label>
+              <input
+                type="text"
+                name="first_name"
+                required
+                value={formData["first_name"]}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Last Name
+              </label>
+              <input
+                type="text"
+                name="last_name"
+                required
+                value={formData["last_name"]}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                required
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Phone Number
+              </label>
+              <input
+                type="tel"
+                name="phone_number"
+                required
+                value={formData["phone_number"]}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                required
+                value={formData.username}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                required
+                value={formData.password}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Date of Birth
+              </label>
+              <input
+                type="date"
+                name="date_of_birth"
+                required
+                value={formData["date_of_birth"]}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Profile Picture URL
+              </label>
+              <input
+                type="text"
+                name="profile_picture"
+                value={formData["profile_picture"]}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-neutral-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-neutral-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                disabled={loading}
+              >
+                {loading ? "Signing up..." : "Sign Up"}
+              </button>
+            </div>
+          </form>
+        </>
+      )}
+      {error && (
+        <h2 className="text-red-400 text-sm mt-2">Error signing up</h2>
+      )}
+      {success && (
+        <h2 className="text-green-400 text-sm mt-2">
+          Successfully signed up
+        </h2>
+      )}
+    </div>
+  );
+}

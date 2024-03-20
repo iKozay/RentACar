056eb5d01bd2d5d904ed0f19b81a0c51f94bf53e
Zeiad Badawy <zeiadbadawy@gmail.com>
2024-03-20 13:07:25 -0400

    frontend: addons section in make reservation form

--- src/frontend/src/components/reservation/ReservationForm.jsx
+++ src/frontend/src/components/reservation/ReservationForm.jsx
@@ -1,25 +1,77 @@
 
 import VehicleViewer from "./VehicleViewer.jsx";
-// import Payment from "./Payment.jsx";
+import Payment from "./Payment.jsx";
+import AddonSelector from "./AddonSelector.jsx";
+import React from "react";
+import {Link} from "react-router-dom";
 
-export default function ReservationForm({selectedVehicle, currentUser}) {
+export default function ReservationForm({selectedVehicle}) {
+    const pickupDate = new Date(localStorage.getItem("startDate")).toISOString().split('T')[0];
+    const returnDate = new Date(localStorage.getItem("endDate")).toISOString().split('T')[0];
+    const dates = `${pickupDate} - ${returnDate} (${computeNumOfDays(pickupDate,returnDate)} days)`;
+    const vehicleTotalPrice = computeTotalPrice(pickupDate, returnDate, selectedVehicle.price);
+    const [addonPrice, setAddonPrice] = React.useState(0);
     return(
         <div className="w-full p-6 my-6 mx-auto bg-white rounded-md shadow-2xl shadow-stone-300 lg:max-w-xl ">
             <table className={""}>
                 <tbody>
                 <tr>
                     <td>
-                        <VehicleViewer vehicle={selectedVehicle}/>
+                        <VehicleViewer vImage={selectedVehicle.Image} price={selectedVehicle.price} make={selectedVehicle.make} dates={dates} vehicleTotalPrice={vehicleTotalPrice}/>
                     </td>
                 </tr>
                 <tr>
-                    {/* <td>
-                        <Payment cancel={goToBrowsing} submit={setReservationBooked} vehicleID={selectedVehicle._id} userID={currentUser.id}/>
-                    </td> */}
+                    <td>
+                        <AddonSelector addonPrice={addonPrice} setAddonPrice={setAddonPrice}/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div className={"mb-5 pl-5"}>
+                            <div className="grid justify-items-end mb-2">
+                                <div className="mb-2 inline-block mr-5">
+                                    <div className="text-stone-600">
+                                        <p className={"float-left mr-2"}>Subtotal:</p>
+                                        <p className={"float-right"}> {(Math.round((vehicleTotalPrice+addonPrice) * 100) / 100).toFixed(2)}$</p>
+                                    </div>
+                                    <div className="text-stone-600">
+                                        <p className={"float-left mr-2"}>Taxes:</p>
+                                        <p className={"float-right"}> {(Math.round((0.14975*(vehicleTotalPrice+addonPrice)) * 100) / 100).toFixed(2)}$</p>
+                                    </div>
+                                    <div className="text-stone-600">
+
+                                        <p className={"float-left mr-2 font-bold text-2xl mt-4"}>Total:</p>
+                                        <p className={"float-right text-2xl mt-4"}> {(Math.round((1.14975*(vehicleTotalPrice+addonPrice)) * 100) / 100).toFixed(2)}$</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <div className="flex justify-between mr-8 ml-8">
+                        <Link to={"/"}>
+                            <button className="bg-blue-500 text-white p-2 rounded-lg w-20 hover:bg-blue-600">Cancel</button>
+                        </Link>
+                        <button className="bg-blue-500 text-white p-2 rounded-lg w-20 hover:bg-blue-600">Pay</button>
+                    </div>
                 </tr>
                 </tbody>
             </table>
         </div>
     );
+}
+
+function computeNumOfDays(fromDate, toDate) {
+    const from = Date.parse(fromDate);
+    const to = new Date(toDate);
+    const diffTime = Math.abs(to - from);
+    const diffDays = Math.round(diffTime / (1000 * 3600 * 24));
+    return diffDays;
+}
+
+
+function computeTotalPrice(fromDate, toDate, price) {
+    return computeNumOfDays(fromDate, toDate) * price;
 }
 
--- src/frontend/src/components/reservation/VehicleViewer.jsx
+++ src/frontend/src/components/reservation/VehicleViewer.jsx
@@ -3,64 +3,45 @@ import {useContext} from "react";
 import {UserContext} from "../../Pages/Root";
 import { Link } from 'react-router-dom';
 import createReservation from "../../utilities/createReservation";
-export default function VehicleViewer({vehicle}) {
-    const {user} = useContext(UserContext);
-    // set vehicle from date to feb 1st 2024
-    // set vehicle to date to feb 6rd 2024
-    //////////////////
-    // Temporary code
-    vehicle.pickupDate = new Date(localStorage.getItem("startDate")).toISOString().split('T')[0];
-    vehicle.returnDate = new Date(localStorage.getItem("endDate")).toISOString().split('T')[0];
-    //////////////////
+export default function VehicleViewer({vImage, dates, make,price,vehicleTotalPrice}) {
+    //const {user} = useContext(UserContext);
     return (
         <div className="flex justify-center">
-                <div className="p-5">
+                <div className="p-3">
                     <div className="block text-sm font-medium justify-end flex text-sky-500 mb-2">
                         <Link to="/">
                             <div className={'hover:text-sky-700 cursor-pointer'}>Change Selection</div>
                         </Link>
                     </div>
                     <div className={'box-border border-inherit border-2 flex justify-center items-center'}>
-                        <img src={vehicle.Image}/>
+                        <img src={vImage}/>
                     </div>
                     <div className={"flex justify-center"}>
-                        <div className="mb-2 text-2xl font-bold tracking-tight">{vehicle.pickupDate} - {vehicle.returnDate} ({computeNumOfDays(vehicle.pickupDate,vehicle.returnDate)} days)</div>
+                        <div className="mb-2 text-2xl font-bold tracking-tight">{dates}</div>
                     </div>
                     <div className={"flex justify-center"}>
                         <div className="mb-3 font-normal text-stone-600 inline-block">
                             <div >
-                                <div className={'block mr-2 mb-2'}>{vehicle.make}</div>
+                                <div className={'block mr-2 mb-2'}>{make}</div>
                             </div>
                             <div className={'flex justify-center'}>
-                                <div className={'block mb-2'}>{vehicle.price}$/day</div>
-                            </div>
-                            <div className={'flex justify-center'}>
-                                {/* <div className={'block mr-2 mb-2'}>Total: </div> */}
-                                {/* <div className={'block mr-2 mb-2'}>{computeTotal(vehicle.fromDate,vehicle.toDate,vehicle.price)}$</div> */}
+                                <div className={'block mb-2'}>{price}$/day</div>
                             </div>
                         </div>
                     </div>
-                    <div className={"flex justify-center pb-1"}>
-                        <Link to={user?`/reservation/confirmation`:'/login'}>
-                            <button className={'bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded'}
-                                onClick={()=>{user? createReservation(vehicle._id,user.id,vehicle.pickupDate,vehicle.returnDate):()=>{}}}> Book Vehicle</button>
-                        </Link>
+                    <div className="grid justify-items-end mb-2">
+                        <div className="mt-2 inline-block">
+                            <div className="text-stone-600 font-semibold">Vehicle: {(Math.round(vehicleTotalPrice * 100) / 100).toFixed(2)}$</div>
+                        </div>
                     </div>
+                    {/*<div className={"flex justify-center pb-1"}>*/}
+                    {/*    <Link to={user?`/reservation/confirmation`:'/login'}>*/}
+                    {/*        <button className={'bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded'}*/}
+                    {/*            onClick={()=>{user? createReservation(vehicle._id,user.id,vehicle.pickupDate,vehicle.returnDate):()=>{}}}> Book Vehicle</button>*/}
+                    {/*    </Link>*/}
+                    {/*</div>*/}
                     <hr className={'border-1 border-stone-300'}/>
                 </div>
         </div>
     );
-}
-
-function computeNumOfDays(fromDate, toDate) {
-    const from = Date.parse(fromDate);
-    const to = new Date(toDate);
-    const diffTime = Math.abs(to - from);
-    const diffDays = Math.round(diffTime / (1000 * 3600 * 24));
-    return diffDays;
-}
-
-
-function computeTotal(fromDate, toDate, price) {
-    return computeNumOfDays(fromDate, toDate) * price;
 }

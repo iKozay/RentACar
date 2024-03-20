c4e6a2dd2dc448e00038678c76591f845b31e672
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-03-20 18:53:40 -0400

    backend: geocode lat and lon for branch in creation

--- src/backend/models/branchModel.js
+++ src/backend/models/branchModel.js
@@ -1,28 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const branchSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   location: {
-    street: String, 
+    street: String,
     postal_code: String,
     city: String,
-    province: String
+    province: String,
   },
-  vehicles: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Vehicle'
-  }]
+  vehicles: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Vehicle",
+    },
+  ],
 });
 
-
-branchSchema.virtual('address').get(function() {
+branchSchema.virtual("address").get(function () {
   return `${this.location.street}, ${this.location.city}, ${this.location.province}`;
 });
 branchSchema.set("toJSON", { getters: true });
-const Branch = mongoose.model('Branch', branchSchema);
+const Branch = mongoose.model("Branch", branchSchema);
 
 module.exports = Branch;

26da67abd57561b2f97ea584f48ffa8ee11cb298
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2024-04-08 01:48:05 -0400

    frontend: add notification sign for issues with new messages

--- src/frontend/src/Pages/CustomerIssues.jsx
+++ src/frontend/src/Pages/CustomerIssues.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useContext } from 'react';
 import fetchData from '../utilities/fetchData';
 import {Link} from 'react-router-dom';
 import { UserContext } from './Root';
+import Issues from './Issues';
 
 export default function CustomerIssues() {
     const {user} = useContext(UserContext);
@@ -32,20 +33,7 @@ export default function CustomerIssues() {
 
     return (
         <div className="container mx-auto p-4">
-            {!error && !loading ? (
-                <div>
-                    {issues.map(issue => (
-                        <Link key={issue._id} to={`/issues/${issue._id}`}><div  className="bg-white shadow-md rounded-md p-4 mb-4">
-                            <h2 className="text-lg font-bold mb-2">{issue.subject}</h2>
-                            <p className="text-gray-600">{issue.description}</p>
-                        </div></Link>
-                    ))}
-                </div>
-            ) : (
-                <div className="p-2 text-center">
-                    {loading && !error ? "Loading..." : "Failed to load customers"}
-                </div>
-            )}
+        <Issues admin={false}/>
         </div>
     );
 }
